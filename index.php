4c16a5830b90d7969915ed3fe98b720835a67f25
pedroparro <pedroparro10@gmail.com>
2022-01-19 20:44:48 -0300

    Atualizando

--- index.php
+++ index.php
@@ -9,47 +9,48 @@
 </head>
 <body>
     <div class="h2">
-        <h2>FORMULÁRIO PHP-MYSQL-AJAX</h2>
+        <h2>FORMULÁRIO VEÍCULOS</h2>
     </div>
     <!--Alert-->
     <div id="alert-form"></div>
-    <!--Alert-->
-    <div id="alert-success"></div>
-    <!--Form-->
+    
     <div class="formulario">
             <form method="POST">
                 <!--nome-->
                 <div class="forms">
-                    <label for="nome">Nome
-                        <input type="text" name="nome" id="nome" placeholder="Digite o Nome" maxlength="50" required>
-                    </label>
+                    <label for="nome">Nome<br>
+                        <input type="text" name="nome" id="nome" placeholder="Digite o Nome do Veículo" maxlength="50" required>
+                    </label><br>
                 </div>
                 <!--cor-->
                 <div class="forms">
-                    <label for="cor">Cor
+                    <label for="cor">Cor<br>
                         <input type="text" name="cor" id="cor" placeholder="Digite a Cor" maxlength="50" required>
                     </label>
                 </div>
                 <!--placa-->
                 <div class="forms">
-                    <label for="placa">Placa
+                    <label for="placa">Placa<br>
                         <input type="text" name="placa" id="placa" placeholder="Digite a Placa" maxlength="50" required>
                     </label>
                 </div>
                 <!--ano-->
                 <div class="forms">
-                    <label for="ano">Ano
+                    <label for="ano">Ano<br>
                         <input type="text" name="ano" id="ano" placeholder="Digite o Ano" maxlength="50" required>
                     </label>
                 </div>
                 <!--cidade-->
                 <div class="forms">
-                    <label for="cidade">Cidade
+                    <label for="cidade">Cidade<br>
                         <input type="text" name="cidade" id="cidade" placeholder="Digite a Cidade" maxlength="50" required>
                     </label>
                 </div>
                 <!--button-->
                 <button type="button" name="submit" id="submit">Cadastrar</button>
+                  <!--Alert-Success-->
+                <div id="alert-success"></div>
+            <!--Form-->
             </form>
     </div>
     <!--script-->
